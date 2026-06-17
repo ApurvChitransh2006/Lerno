a9ec645ac92a6b6d35fef13b782b00540738a613
ApurvChitransh2006 <apurvchitransh.in@gmail.com>
2026-06-17 14:18:56 +0530

    fix: TRPC Hydration Helper

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
+import { createCallerFactory, createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 
 /**
  * This is the primary router for your server.
@@ -7,6 +7,9 @@ import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
 	// post: postRouter,
+	health: publicProcedure.query(() => {
+		return "Server TRPC is healthy!";
+	}),
 });
 
 // export type definition of API
--- start-database.sh
+++ start-database.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+git branch -M main#!/usr/bin/env bash
 # Use this script to start a docker container for a local development database
 
 # TO RUN ON WINDOWS:
